a3ec32f9d964aa46c22a57dada4e2983edde8300
saul-c <saul.chen@hotmail.com>
2015-06-21 18:30:11 +0800

    Revert "abc"
    This reverts commit fc2e6b7c7a98811de02e9a451849a87a86758fca.

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,6 +5,6 @@ public class Test{
 		System.out.println("第一次使用github");
 		System.out.println("更新了！");
 		System.out.println("修复了一个bug！");
-		System.out.println("又修复了一个bug！！");
+		System.out.println("又修复了一个bug！");
 	}
 }
